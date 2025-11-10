8f8a5162ffde537a18bc404f063d4c66a1e3fffa
aojunhao123 <1844749591@qq.com>
2025-11-11 00:09:31 +0800

    add test with snapshot

--- tests/listy.test.tsx
+++ tests/listy.test.tsx
@@ -1,10 +1,24 @@
-import React from "react";
-import Listy from "@rc-component/listy";
-import { render, screen } from "@testing-library/react";
+import React from 'react';
+import Listy from '@rc-component/listy';
+import { render, screen } from '@testing-library/react';
 
-describe("Listy", () => {
-  it("should render", () => {
-    render(<Listy items={[{ id: 1 }]} rowKey="id" itemRender={(item) => <div>{item.id}</div>} />);
-    expect(screen.getByText("1")).toBeInTheDocument();
+describe('Listy', () => {
+  const renderListy = () =>
+    render(
+      <Listy
+        items={[{ id: 1 }]}
+        rowKey="id"
+        itemRender={(item) => <div>{item.id}</div>}
+      />,
+    );
+
+  it('should render', () => {
+    renderListy();
+    expect(screen.getByText('1')).toBeInTheDocument();
+  });
+
+  it('should match snapshot', () => {
+    const { asFragment } = renderListy();
+    expect(asFragment()).toMatchSnapshot();
   });
 });
